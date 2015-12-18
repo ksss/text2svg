d74eb9ee18b6f920cf1ac1754d9057187f83a32d
ksss <co000ri@gmail.com>
2015-12-18 17:27:45 +0900

    Add CLI Option fill

--- lib/text2svg/cli.rb
+++ lib/text2svg/cli.rb
@@ -2,10 +2,10 @@ require 'text2svg/typography'
 
 module Text2svg
   module CLI
-    Option = Struct.new(:font, :text_align)
+    Option = Struct.new(:font, :text_align, :fill)
 
     def start
-      o = CLI::Option.new
+      o = CLI::Option.new(nil, :left, :black)
       OptionParser.new.tap { |opt|
         opt.on('-f', '--font [FONT]', 'font file path (require)') do |arg|
           o.font = arg
@@ -13,9 +13,12 @@ module Text2svg
         opt.on('--text-align [ALIGN]', 'text align left,right or center (default left)') do |arg|
           o.text_align = arg
         end
+        opt.on('--fill [FILL]', 'text fill color (default black)') do |arg|
+          o.fill = arg
+        end
       }.parse!(ARGV)
       text = ARGV[0] || $stdin.read
-      puts Text2svg::Typography.build(text, font: o.font, text_align: o.text_align)
+      puts Text2svg::Typography.build(text, font: o.font, text_align: o.text_align, stroke: :none, fill: o.fill)
     end
     module_function :start
   end
