7ab00bc35e3d3be8d9233f0df9b2c9dcdb35f6e9
ksss <co000ri@gmail.com>
2015-12-18 17:12:49 +0900

    Add text-align option for CLI

--- lib/text2svg/cli.rb
+++ lib/text2svg/cli.rb
@@ -2,7 +2,7 @@ require 'text2svg/typography'
 
 module Text2svg
   module CLI
-    Option = Struct.new(:font)
+    Option = Struct.new(:font, :text_align)
 
     def start
       o = CLI::Option.new
@@ -10,9 +10,12 @@ module Text2svg
         opt.on('-f', '--font [FONT]', 'font file path (require)') do |arg|
           o.font = arg
         end
+        opt.on('--text-align [ALIGN]', 'text align left,right or center (default left)') do |arg|
+          o.text_align = arg
+        end
       }.parse!(ARGV)
       text = ARGV[0] || $stdin.read
-      puts Text2svg::Typography.build(text, font: o.font)
+      puts Text2svg::Typography.build(text, font: o.font, text_align: o.text_align)
     end
     module_function :start
   end
--- lib/text2svg/cli_test.rb
+++ lib/text2svg/cli_test.rb
@@ -3,15 +3,21 @@ require 'text2svg/cli'
 module Text2svgCLITest
   def test_start(t)
     ARGV[0] = 'abc'
-    ARGV[1] = '--font=/Library/Fonts/Times New Roman.ttf'
-    out = capture do
-      Text2svg::CLI.start
-    end
-    unless String === out
-      t.error 'return value was break'
-    end
-    unless 0 < out.length
-      t.error 'unsupported font?'
+    %w(left right center).each do |text_align|
+      ARGV[1] = '--font=/Library/Fonts/Times New Roman.ttf'
+      ARGV[2] = "--text-align=#{text_align}"
+      out = capture do
+        Text2svg::CLI.start
+      end
+      unless String === out
+        t.error 'return value was break'
+      end
+      unless 0 < out.length
+        t.error 'unsupported font?'
+      end
+      if out.strip[0,4] != "<svg"
+        t.error 'output format was break'
+      end
     end
   end
 
