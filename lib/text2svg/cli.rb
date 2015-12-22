3d544e59e95a2ba4164ccc2f516f37c66815c9ab
ksss <co000ri@gmail.com>
2015-12-22 14:42:11 +0900

    Add comment

--- lib/text2svg/cli.rb
+++ lib/text2svg/cli.rb
@@ -5,16 +5,16 @@ module Text2svg
   module CLI
     def start
       o = Option.new(
-        nil,
-        :left,
-        :black,
-        :none,
-        1,
-        Encoding::UTF_8,
-        :none,
-        1,
-        false,
-        false,
+        nil,             # font
+        :left,           # text_align
+        :black,          # fill
+        :none,           # stroke
+        1,               # stroke_width
+        Encoding::UTF_8, # encoding
+        :none,           # stroke
+        1,               # stroke_width
+        false,           # bold
+        false,           # italic
       )
       OptionParser.new.tap { |opt|
         opt.on('-f', '--font FONT', 'font file path (require)') do |arg|
