f338889c1970a582c37156b03b9d727d51a42bd6
ksss <co000ri@gmail.com>
2016-03-04 15:37:42 +0900

    Use free font for testing

--- lib/text2svg/typography_test.rb
+++ lib/text2svg/typography_test.rb
@@ -14,34 +14,35 @@ module Text2svgTypographyTest
       t.error('error conditions was changed')
     end
 
-    opt = Text2svg::Option.new(
-      '/Library/Fonts/Times New Roman.ttf',
-      nil,
-    )
-    [nil, :left, :right, :center].each do |text_align|
-      opt.text_align = text_align
-      [Encoding::UTF_8, Encoding::ASCII_8BIT].each do |encoding|
-        opt.encoding = encoding
-        [nil, 'fill="red"'].each do |attribute|
-          opt.attribute = attribute
-          [false, true].each do |bold|
-            opt.bold = bold
-            [false, true].each do |italic|
-              opt.italic = italic
-              ['ABC', "\n", "\n\nA", "", "A\nB\n\n", "A\n\n\nC", "<", ">", "&", "=", "@", "%", "#", "("].each do |text|
-                begin
-                  c = Text2svg::Typography.build(text, opt)
-                rescue => e
-                  t.log("raise error #{e.class}: #{e.message} with text=\"#{text}\",opt=#{opt}")
-                  raise
-                end
+    Dir["data/*"].grep(/otf|ttf/).each do |font|
+      [nil, :left, :right, :center].each do |text_align|
+        [Encoding::UTF_8, Encoding::ASCII_8BIT].each do |encoding|
+          [nil, 'fill="red"'].each do |attribute|
+            [false, true].each do |bold|
+              [false, true].each do |italic|
+                opt = Text2svg::Option.new
+                opt.font = font
+                opt.text_align = text_align
+                opt.encoding = encoding
+                opt.attribute = attribute
+                opt.bold = bold
+                opt.italic = italic
 
-                unless Text2svg::Content === c
-                  t.error('return value was break')
-                end
+                ['ABC', "\n", "\n\nA", "", "A\nB\n\n", "A\n\n\nC", "<", ">", "&", "=", "@", "%", "#", "("].each do |text|
+                  begin
+                    c = Text2svg::Typography.build(text, opt)
+                  rescue => e
+                    t.log("raise error #{e.class}: #{e.message} with text=\"#{text}\",opt=#{opt}")
+                    raise
+                  end
 
-                unless c.data.encoding == Encoding::UTF_8
-                  t.error('encoding was changed')
+                  unless Text2svg::Content === c
+                    t.error('return value was break')
+                  end
+
+                  unless c.data.encoding == Encoding::UTF_8
+                    t.error('encoding was changed')
+                  end
                 end
               end
             end
